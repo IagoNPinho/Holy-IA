00255bc5774f8437d096d9f898d5ca5a57526bed
Iago Pinho <iago.pinho.dev@gmail.com>
2026-03-17 00:46:08 -0300

    update env

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -13,7 +13,9 @@ module.exports = {
         WHATSAPP_PROVIDER: "waha",
         WAHA_BASE_URL: "http://127.0.0.1:3000",
         WAHA_API_KEY: "8bf936bf39444f8fa5ca30ebeb97f166",
-        WAHA_SESSION: "default"
+        WAHA_SESSION: "default",
+        WAHA_MESSAGES_ENDPOINT: "/api/messages",
+        WAHA_MESSAGES_METHOD: "POST"
       },
       error_file: "../logs/holy-ai-backend-error.log",
       out_file: "../logs/holy-ai-backend-out.log",
